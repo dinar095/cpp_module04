53ff9f000b762f4ab6066c8ef3c8f8558094488a
Desausag <dinar095@hotmail.com>
2021-09-24 15:07:04 +0300

    ex01 need add random think

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,6 +1,7 @@
 #include "Animal.hpp"
 #include "Dog.hpp"
 #include "Cat.hpp"
+#define RAND_MAX 5
 
 int main()
 {
@@ -8,4 +9,8 @@ int main()
 	const Animal* i = new Cat();
 	delete j;//should not create a leak
 	delete i;
+	int tt;
+	srand(std::time(0));
+	tt = std::rand() % 20;
+	cout << tt << endl;
 }
