e1c8839ba02030b53892b57e5a6af94d3d5b4846
dinar <dinar95@mail.ru>
2021-09-25 21:35:45 +0300

    ex03 Cure done

--- ex03/Cure.cpp
+++ ex03/Cure.cpp
@@ -6,8 +6,23 @@ Cure::Cure(string const &type) : AMateria(type) {}
 
 Cure& Cure::operator=(const Cure &src)
 {
-	if (*this == src)
+	if (this == &src)
 		return *this;
 	else
 		type = src.getType();
+}
+Cure::Cure(const Cure &src)
+{
+	*this = src;
+}
+
+AMateria* Cure::clone() const
+{
+	AMateria* clone = new Cure(this->type);
+	return clone;
+}
+
+void Cure::use(ICharacter& target)
+{
+	cout <<  "* heals " << target.getName() << " wounds *" << endl;
 }
